1cd2d24d915a41ef1e0e877bff168d7eaceea25c
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-02-06 14:47:40 +0100

    added docstring to charge_density_monopole

--- halex/std/integrals.py
+++ halex/std/integrals.py
@@ -1,10 +1,25 @@
 import torch
-
-from .utils import isqrtm
+from .utils import sqrtm
 
 
 def charge_density_monopole(ovlp, natm, ao_labels, mo_coeff_a, mo_coeff_b):
-    ovlp_i12 = isqrtm(ovlp)
+    """computes the q_pq^A using Löwdin population analysis.
+
+        q_pq^A = sum_(μ ϵ A) C'μp^(a) C'μq^(b)
+        C' = S^(½) C
+
+    Args:
+        ovlp (torch.Tensor, (n_ao, n_ao)): AO overlap matrix (S).
+        natm (int): number of atoms.
+        ao_labels (list): list of tuples describing AOs.
+                          same as calling mol.ao_labels(fmt=None) from pyscf.
+                          tuple fmt: (atom_index: int, atom: str, ao_name: str, m_def: str)
+        mo_coeff_a (torch.Tensor, (n_ao_a, n_mo_a)): MO coefficients matrix (C).
+        mo_coeff_b (torch.Tensor, (n_ao_b, n_mo_b)): MO coefficients matrix (C).
+    Returns:
+        q (torch.Tensor, (natm, n_mo_a, n_mo_b)): charges from Löwdin population analysis.
+    """
+    ovlp_i12 = sqrtm(ovlp)
     coeff_orth_a = torch.matmul(ovlp_i12, mo_coeff_a)
     coeff_orth_b = torch.matmul(ovlp_i12, mo_coeff_b)
     nmo_a = coeff_orth_a.shape[1]
