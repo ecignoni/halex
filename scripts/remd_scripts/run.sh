501138f396bd9523c69f972c4065a88bb44c41d8
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-05-03 11:35:00 +0200

    improved run.sh

--- scripts/remd_scripts/run.sh
+++ scripts/remd_scripts/run.sh
@@ -7,6 +7,17 @@
 #PBS -e ipi_run.err
 
 
+#: Options
+# For DFTB+, avoid multiple instances of DFTB+ to
+# overlap by limiting the number of omp threads
+export OMP_NUM_THREADS=2
+WorkDir="/local/e.cignoni/ipi/styrene"
+NReplicas=16
+LogFile=ipi_driver.log
+OrigDir="$PBS_O_WORKDIR"
+
+
+#: Functions
 log_date() {
     msg=$1
     logfile=$2
@@ -15,46 +26,60 @@ log_date() {
     echo "@SIMULATION $msg AT: $date" >> $logfile
 }
 
+create_folder() {
+    local folder=$1
+
+    if [ ! -d $folder ] ; then
+        mkdir -p $visual
+    fi
+}
+
+create_dftb_folders_and_copy_input() {
+    for idx in `seq 0 $(($NReplicas - 1))` ; do
+        if [ ! -d dftb_${idx} ] ; then
+            mkdir dftb_${idx}
+        fi
+        cp dftb_in.hsd dftb_${idx}
+        cp init.gen dftb_${idx}
+    done
+}
+
+
+delete_folder_if_success() {
+    local exitcode=$1
+    local folder=$2
+
+    if [ $exitcode -ne 0 ] ; then
+        echo "ERROR: Error while copying back files from node" >> $LogFile
+        echo "ERROR: Not deleting the local folder." >> $LogFile
+    else
+        rm -r $folder
+    fi
+}
+
+#: Run
 
 # source conda
-source /home/e.cignoni/software/miniconda3/etc/profile.d/conda.sh
-
 # environment with DFTB+ and i-PI
+source /home/e.cignoni/software/miniconda3/etc/profile.d/conda.sh
 conda activate dftbp
 
-cat $PBS_NODEFILE > ${PBS_O_WORKDIR}/pbs_node
-
-# copy the folder locally on the node and work there
-workdir="/local/e.cignoni/ipi/styrene"
-origdir="$PBS_O_WORKDIR"
+# where are we running
+cat $PBS_NODEFILE > ${OrigDir}/pbs_node
 
 # create local folder if not present
-if [ ! -d $workdir ] ; then
-    mkdir -p $workdir
-fi
+crate_folder $WorkDir
 
 # copy content locally on the node
-rsync -a $origdir/* $workdir/
+rsync -a $OrigDir/* $WorkDir/
 
 # go local on the node
-cd $workdir
-
-# For DFTB+, avoid multiple instances of DFTB+ to
-# overlap
-export OMP_NUM_THREADS=2
-NReplicas=16
-LogFile=ipi_driver.log
+cd $WorkDir
 
 # Create a separate folder for each dftb+ instance
-for idx in `seq 0 $(($NReplicas - 1))` ; do
-    if [ ! -d dftb_${idx} ] ; then
-        mkdir dftb_${idx}
-    fi
-    cp dftb_in.hsd dftb_${idx}
-    cp init.gen dftb_${idx}
-done
+create_dftb_folders_and_copy_input
 
-
+# initialize log
 echo "" > $LogFile
 
 log_date STARTED $LogFile
@@ -66,14 +91,9 @@ i-pi input.xml >> $LogFile & sleep 1; for idx in `seq 0 $(($NReplicas - 1))` ; d
 log_date FINISHED $LogFile
 
 # copy back the simulation
-rsync -a $workdir/* $origdir/
+rsync -a $WorkDir/* $OrigDir/
 
 # if you cannot copy back the results, do not remove the local folder and report the problem
-if [ $? -ne 0 ] ; then
-    echo "ERROR: Error while copying back files from node" >> $LogFile
-    echo "ERROR: Not deleting the local folder." >> $LogFile
-else
-    rm -r $workdir
-fi
+delete_folder_if_success $? $WorkDir
 
 exit 0
