c22377df3d008020bdac8a277ce295d8794848b2
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-05-16 13:23:28 +0200

    fixed import

--- halex/hamiltonian/__init__.py
+++ halex/hamiltonian/__init__.py
@@ -1,3 +1,3 @@
 from .cg_coupling import couple_blocks, decouple_blocks
-from .features import compute_ham_features, drop_unused_features
+from .features import compute_ham_features, drop_unused_features, drop_noncore_features
 from .tensormap_dense import dense_to_blocks, blocks_to_dense
